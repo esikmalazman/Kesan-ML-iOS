6e42d0135c9971c23dd3755922fc093e80af59e7
Ikmal Azman <opikmalazman@gmail.com>
2021-09-15 19:54:08 +0800

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by Ikmal Azman on 15/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
