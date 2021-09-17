96c68120b2461377864296e12989170a8f468543
Ikmal Azman <opikmalazman@gmail.com>
2021-09-17 23:51:41 +0800

    initialise app theme color in constants

---
+++ Kesan/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SmartCamera
+//
+//  Created by Ikmal Azman on 17/09/2021.
+//
+
+import UIKit
+
+//MARK:- App Color Theme
+extension UIColor {
+    static let lightOrange = UIColor(hexString: "#FBEBD3")
+    static let secondaryOrange = UIColor(hexString: "#FBB747")
+    static let  primaryOrange = UIColor(hexString: "#F16A34")
+}
